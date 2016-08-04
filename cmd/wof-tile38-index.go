f8dbf113d80e7527b072099d952d8be3032cd30e
thisisaaronland <whosonfirst@mapzen.com>
2016-08-04 20:00:52 +0000

    append hierarchy - mostly to better understand FIELDS etc

--- cmd/wof-tile38-index.go
+++ cmd/wof-tile38-index.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/garyburd/redigo/redis"
+	gabs "github.com/jeffail/gabs"
 	"github.com/whosonfirst/go-whosonfirst-crawl"
 	"github.com/whosonfirst/go-whosonfirst-geojson"
 	"log"
@@ -64,7 +65,37 @@ func main() {
 
 		defer conn.Close()
 
-		_, err = conn.Do("SET", *collection, str_wofid, "OBJECT", str_geom)
+		/*
+			cmd := "SET"
+			args:= make([]interface{}, 0)
+
+			args = append(args, *collection)
+			args = append(args, str_wofid)
+			args = append(args, "OBJECT")
+			args = append(args, str_geom)
+		*/
+
+		h := MakeHierarchy(body)
+
+		country_id, ok := h["country_id"]
+
+		if !ok {
+			country_id = -1
+		}
+
+		region_id, ok := h["region_id"]
+
+		if !ok {
+			region_id = -1
+		}
+
+		locality_id, ok := h["locality_id"]
+
+		if !ok {
+			locality_id = -1
+		}
+
+		_, err = conn.Do("SET", *collection, str_wofid, "FIELD", "country_id", country_id, "FIELD", "region_id", region_id, "FIELD", "locality_id", locality_id, "OBJECT", str_geom)
 
 		if err != nil {
 			log.Printf("SET error %v\n", err)
@@ -74,16 +105,23 @@ func main() {
 		// http://tile38.com/commands/set/
 		// http://tile38.com/commands/fset/
 
-		/*
-			placetype := feature.Placetype()
-			key := str_wofid + ":placetype"
+		placetype := feature.Placetype()
+		placetype_key := str_wofid + ":placetype"
 
-			_, err = conn.Do("SET", "whosonfirst", key, "STRING", placetype)
+		_, err = conn.Do("SET", *collection, placetype_key, "STRING", placetype)
 
-			if err != nil {
-				fmt.Printf("FAILED to set placetype on %s because, %v\n", key, err)
-			}
-		*/
+		if err != nil {
+			fmt.Printf("FAILED to set placetype on %s because, %v\n", placetype_key, err)
+		}
+
+		name := feature.Name()
+		name_key := str_wofid + ":name"
+
+		_, err = conn.Do("SET", *collection, name_key, "STRING", name)
+
+		if err != nil {
+			fmt.Printf("FAILED to set name on %s because, %v\n", name_key, err)
+		}
 
 		return nil
 	}
@@ -93,4 +131,34 @@ func main() {
 
 	_ = c.Crawl(cb)
 
+}
+
+func MakeHierarchy(body *gabs.Container) map[string]int64 {
+
+	h := make(map[string]int64)
+
+	hiers, err := body.Path("properties.wof:hierarchy").Children()
+
+	if err != nil {
+		return h
+	}
+
+	if len(hiers) == 0 {
+		return h
+	}
+
+	possible, err := hiers[0].ChildrenMap()
+
+	if err != nil {
+		return h
+	}
+
+	for ancestor, v := range possible {
+
+		wofid := v.Data().(float64)
+
+		h[ancestor] = int64(wofid)
+	}
+
+	return h
 }
