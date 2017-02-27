76d91a3f3f041813bdc4369accc1db2622a06710
thisisaaroland <aaron@mapzen.com>
2017-02-27 08:47:04 -0800

    actually make it work... maybe?

--- whosonfirst/results.go
+++ whosonfirst/results.go
@@ -1,23 +1,23 @@
 package whosonfirst
 
 import (
-	"github.com/whosonfirst/go-whosonfirst-tile38/whosonfirst"
+	"github.com/whosonfirst/go-whosonfirst-tile38"
 )
 
 func Tile38ResponseToWOFResults(rsp tile38.Tile38Response) (tile38.WOFResults, error) {
 
 	wof_results := make([]tile38.WOFResult, 0)
 
-	for _, p := range r.Points {
+	for _, p := range rsp.Points {
 
 		tmp := make(map[string]int64)
 
-		for i, k := range r.Fields {
+		for i, k := range rsp.Fields {
 			v := int64(p.Fields[i].(float64))
 			tmp[k] = v
 		}
 
-		wof_result := WOFResult{
+		wof_result := tile38.WOFResult{
 			WOFID:          tmp["wof:id"],
 			WOFParentID:    tmp["wof:parent_id"],
 			WOFPlacetypeID: tmp["wof:placetype_id"],
